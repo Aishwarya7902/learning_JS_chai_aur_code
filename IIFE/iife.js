aa1f980bebc8371096c3ae2c859480cc3d8133cb
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-01-13 09:03:50 -0800

    Update iife.js

--- IIFE/iife.js
+++ IIFE/iife.js
@@ -1,5 +1,8 @@
 // Immediately Invoked Function Expressions (IIFE)
 
+// if u wanna write two IIFE in a same file u will have to use ;
+
+
 
 (function chai(){
     // named IIFE
@@ -8,4 +11,4 @@
 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
-} )('hitesh')
+} )('hitesh') //if u want to pass arguments.....u can use this syntax
