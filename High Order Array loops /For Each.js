e9f2f98beaef97fe937c2351208c3b0750891117
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-01-28 06:11:09 -0800

    Create For Each.js

---
+++ High Order Array loops /For Each.js
@@ -0,0 +1,40 @@
+//for each is used with Array
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach( function (val){ // for each takes a callback function
+//     console.log(val);
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// } )
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);
+} )
