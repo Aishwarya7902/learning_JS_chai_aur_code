a9b4c9462e072adfc9b67df92553307973863926
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-01-28 03:32:37 -0800

    Create For In Loop.js

---
+++ High Order Array loops /For In Loop.js
@@ -0,0 +1,35 @@
+/*
+For In Loops can be used :
+Objects  ✅
+Arrays ✅
+Maps ❌
+
+Note :❤️ For In Loop gives key in case of object and index in case of arrays.
+*/
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {  //it will give error as maps can't be used with for in loop
+//     console.log(key);
+// }
