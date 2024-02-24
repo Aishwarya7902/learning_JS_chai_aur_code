a5433d37e8dd856f7a4dc6fb5d48e4792cdab524
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-02-24 01:03:58 -0800

    Update For Each.js

--- High Order Array loops /For Each.js
+++ High Order Array loops /For Each.js
@@ -1,3 +1,13 @@
+Array ✅
+Objects ❌
+
+/*
+The forEach method is a built-in function in JavaScript that is used to iterate over the elements of an array. 
+It takes a callback function as an argument and applies that function to each element in the array. 
+However, it cannot be directly used with objects because objects in JavaScript do not have a built-in method like arrays do.
+*/
+
+
 //for each is used with Array
 const coding = ["js", "ruby", "java", "python", "cpp"]
 
